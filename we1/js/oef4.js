76483ef3073de7dba6c1dca8ce7b8143538e9618
Mats <mats.mardaga@student.ehb.be>
2022-06-18 21:29:42 +0200

    idgaf

--- we1/js/oef4.js
+++ we1/js/oef4.js
@@ -1,1 +1,16 @@
 'use strict';
+
+function controleerLeeftijd(event){
+  event.preventDefault();
+
+  let age = document.getElementById('age').value;
+  if(age >= 18){
+    console.log('welcome feller');
+  }
+  else{
+    console.log("You are too too young");
+  }
+}
+
+const form = document.getElementById('form');
+form.addEventListener('submit', controleerLeeftijd);
