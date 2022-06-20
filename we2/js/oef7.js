0ea2baade1eeb7996390c48a63fd5c7e9e00551b
Mats <mats.mardaga@student.ehb.be>
2022-06-20 20:52:41 +0200

    Oef7 we2

--- we2/js/oef7.js
+++ we2/js/oef7.js
@@ -1,52 +1,63 @@
 'use strict';
 
-window.onload = function(){
+window.onload = function () {
+    document.getElementById('btn1').addEventListener('click', askDetails);
+    document.getElementById('btn2').addEventListener('click', showStudents);
 
-let student = {
-    fname: 'Default',
-    lname: ' default',
-    age: 0,
-    degree: 'default',
-    courses: [],
+    let listOfStudent = [];
 
-    setDetails(details) {
-        [this.fname, this.lname, this.age, this.degree] = details;
-    },
-    setCourses(...courses) {
-        this.courses = [...this.courses, ...courses];
+    function Student(fname, lname, age, degree) {
+        this.fname = fname;
+        this.lname = lname;
+        this.age = age;
+        this.degree = degree;
+        this.courses = [];
 
-    },
-    showDetails() {
-        console.log(`The student is called: ${this.fname} ${this.lname}. \nHe/she is ${this.age} years old and studies ${this.degree}.`);
-        console.log(`His/her courses are: ${this.courses.join(', ')}.`);
+        this.setDetails = function (details) {
+            [this.fname, this.lname, this.age, this.degree] = details;
+        };
+
+        this.setCourses = function (...courses) {
+            this.courses = [...this.courses, ...courses];
+        };
+
+        this.showDetails = function () {
+            let txt = `The student is called ${this.fname} ${this.lname}. \nHe/she is ${this.age} years old and studies ${this.degree}.
+            His/her courses are: ${this.courses.join(', ')}.`;
+            return txt;
+        };
     }
-};
 
-function askCourses() {
-    while (true) {
-        let c = prompt('Course:');
-        if (c) {
-            student.setCourses(c);
+    function askDetails() {
+        let data = [];
+
+        data.push(prompt('First name:'))
+        data.push(prompt('Last name:'))
+        data.push(prompt('Age:'))
+        data.push(prompt('Degree:'))
+
+        let student = new Student();
+        student.setDetails(data);
+
+        while (true) {
+            let c = prompt('Course:');
+            if (c) {
+                student.setCourses(c);
+            }
+            else {
+                break;
+            }
         }
-        else {
-            break;
-        }
+
+        listOfStudent.push(student);
     }
-}
 
-function askDetails() {
-    let data = [];
+    function showStudents(){
+        for(let s of listOfStudent){
+            let p = document.createElement('p');
+            p.innerHTML = s.showDetails();
+            document.getElementById('content').appendChild(p);
+        }
 
-    data.push(prompt('First name:'))
-    data.push(prompt('Last name:'))
-    data.push(prompt('Age:'))
-    data.push(prompt('Degree:'))
-    student.setDetails(data);
-    askCourses();
-    student.showDetails();
-}
-
-document.getElementById('btn1').addEventListener('click', askDetails);
-
-
+    }
 }
