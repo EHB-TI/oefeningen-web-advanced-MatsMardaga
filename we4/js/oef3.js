b169f58a33f0e25a13ae42aa752af98e1404c5e3
Mats <mats.mardaga@student.ehb.be>
2022-06-21 02:25:00 +0200

    we4

--- we4/js/oef3.js
+++ we4/js/oef3.js
@@ -1,7 +1,31 @@
 'use strict'
+let config = null;
+
+async function getConfig(){
+
+    let resp = await fetch('oef3.json');
+    let data = await resp.json();
+    let config = data;
+}
 
 window.onload = () =>{
 
+    getConfig();
 
+    async function getData(param){
+        let url = `${oef3.baseurl}s=${param}${oef3.apikey}`;
+        let resp = await fetch(url);
+        let data = await resp.json();
+        console.log(data);
+    }
+
+    let submitForm = event =>{
+        event.preventDefault();
+        let value = document.getElementById('inputTitle').value;
+        let result = getData(value);
+    }
+
+    document.getElementById('searchform').addEventListener('submit', submitForm);
+    document.getElementById('moviecard').style.display = 'none';
 
 }
